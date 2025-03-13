9459b2cca44695d8a98be063284e6d6248afbb88
dk-willing <davidkarikari89@gmail.com>
2025-03-13 20:21:17 +0000

    Add black-mode and local Storage to save user's theme selection

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const userProfile = document.getElementById("user-profile");
 const settingMenu = document.querySelector(".setting-menu");
+const themeBtn = document.getElementById("theme-btn");
 
 const toggleSettingsMenu = () => {
   settingMenu.classList.toggle("setting-menu-height");
 };
 
+const switchTheme = () => {
+  themeBtn.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-theme");
+
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
+
+function getLocalStorage() {
+  if (localStorage.getItem("theme") == "light") {
+    themeBtn.classList.remove("dark-mode");
+    document.body.classList.remove("dark-theme");
+  } else if (localStorage.getItem("theme") == "dark") {
+    themeBtn.classList.add("dark-mode");
+    document.body.classList.add("dark-theme");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
 userProfile.addEventListener("click", toggleSettingsMenu);
+themeBtn.addEventListener("click", switchTheme);
+document.addEventListener("DOMContentLoaded", getLocalStorage);
